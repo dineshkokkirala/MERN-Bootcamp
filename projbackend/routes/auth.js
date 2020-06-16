556126be40a36d890fbf6883fb7ac4abb9951a71
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-06-16 17:10:03 +0530

    Signin completed

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 
 router.post(
   "/signup",
@@ -14,6 +14,17 @@ router.post(
     }),
   ],
   signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password field is required").isLength({
+      min: 6,
+    }),
+  ],
+  signin
 );
 
 router.get("/signout", signout);
