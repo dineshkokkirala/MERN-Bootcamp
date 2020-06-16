ede3f04e6f4b13a56967220344332025ad8a1f00
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-06-16 16:24:04 +0530

    Signup completed

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,29 @@
+const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
+
 exports.signout = (req, res) => {
   res.json({
     msg: "User signout",
+  });
+};
+
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
+
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in Database",
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
   });
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-const { signout } = require("../controllers/auth");
+const { signout, signup } = require("../controllers/auth");
+
+router.post(
+  "/signup",
+  [
+    check("name", "Name should be atleast 3 characters").isLength({ min: 3 }),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password should be atleast 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  signup
+);
 
 router.get("/signout", signout);
 
