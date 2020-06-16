8fe823fe04ca7d80df8e156ca04dc0a7b9736f0c
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-06-16 11:17:39 +0530

    DB Error resolved

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 const mongoURI = config.get("mongoURI");
 mongoose
-  .connect(mongoURI, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -19,6 +22,10 @@ mongoose
   .catch(() => {
     console.log("DB Connection failed");
   });
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = 8000 || process.env.PORT;
 
