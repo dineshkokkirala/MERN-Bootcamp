845a45cfdd6441aa8e1f4d39cca2109fff3355e5
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-06-17 12:35:01 +0530

    Product creation done

---
+++ projbackend/controllers/product.js
@@ -0,0 +1,53 @@
+const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+
+exports.getProductById = (req, res, next, id) => {
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({ error: "Product not found" });
+      }
+      req.product = product;
+      next();
+    });
+};
+
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({ error: "Problem with Image" });
+    }
+
+    //destructure the fields
+    const { price, name, description, category, stock } = fields;
+
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({ error: "Please include all fields" });
+    }
+
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({ error: "File size is too big" });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //save to DB
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({ error: "Saving product in DB failed" });
+      }
+      res.json(product);
+    });
+  });
+};
