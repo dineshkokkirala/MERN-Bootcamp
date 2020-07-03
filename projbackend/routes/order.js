26adce75b8258fbb3a7b1cf523dfbaa400dc425c
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-03 12:31:51 +0530

    Order created

---
+++ projbackend/routes/order.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
+
+const { getOrderById, createOrder } = require("../controllers/order");
+
+//params
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+//routes
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
+
+module.exports = router;
