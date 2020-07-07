5f3df4c8de796417d6f3e915a94e2e29a955de49
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-07 18:38:11 +0530

    Auth functionalities

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -6,7 +6,7 @@ import Base from "./Base";
 function Home() {
   console.log("API IS", API);
   return (
-    <Base>
+    <Base title="Home Page" description="Welcome to the T-Shirt store">
       <div className="row">
         <div className="col-4">
           <button className="btn btn-success">Test</button>
