52b23787f8d6d31ecce8ecb1e16a67eb8b18879c
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-07 17:05:38 +0530

    Base component setup

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
 
 function Home() {
+  console.log("API IS", API);
   return (
-    <div>
-      <h1>Hello! This is Dinesh</h1>
-    </div>
+    <Base>
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+      </div>
+    </Base>
   );
 }
 
