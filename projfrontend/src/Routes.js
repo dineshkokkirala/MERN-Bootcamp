6fb1dbb2b609a076fd99d51d56e387d01757dcf7
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-08 16:38:43 +0530

    Create Categories

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -8,6 +8,7 @@ import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import AdminRoutes from "./auth/helper/AdminRoutes";
+import AddCategory from "./admin/AddCategory";
 
 function Routes() {
   return (
@@ -18,6 +19,11 @@ function Routes() {
         <Route path="/signin" exact component={Signin} />
         <PrivateRoutes path="/user/dashboard" exact component={UserDashBoard} />
         <AdminRoutes path="/admin/dashboard" exact component={AdminDashBoard} />
+        <AdminRoutes
+          path="/admin/create/category"
+          exact
+          component={AddCategory}
+        />
       </Switch>
     </BrowserRouter>
   );
