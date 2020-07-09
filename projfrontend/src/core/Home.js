998d6b6437d6c4ee27d3dcf59505c57f5845c560
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-09 10:47:21 +0530

    Home page setup

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
 import Card from "./Card";
+import { getProducts } from "./helper/coreapicalls";
 
 function Home() {
   //console.log("API IS", API);
 
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
   return (
     <Base title="Home Page" description="Welcome to the T-Shirt store">
       <div className="row text-center">
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">Test</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">Test</button>
+        <h1 className="text-white">All of T-Shirts</h1>
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+                <Card />
+              </div>
+            );
+          })}
         </div>
       </div>
     </Base>
